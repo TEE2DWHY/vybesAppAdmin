b1dfde3c76d05414c8d4e651c969166c19ffe1d9
TEE2DWHY <isaacolorunfemi330@gmail.com>
2025-02-16 19:55:36 +0100

    created the register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -20,14 +20,14 @@ const page = () => {
       />
       <div className="flex flex-col items-center justify-center h-[100vh] font-[family-name:var(--font-geist-mono)] capitalize text-2xl">
         <h1 className="font-semibold mb-4 text-[24px] uppercase text-center lg:text-3xl">
-          Admin Dashboard.
+          Create An Account.
         </h1>
         <form className="bg-white rounded-xl py-5 px-3 gap-2 flex flex-col w-[90%] sm:w-[55%] lg:w-[35%] shadow-lg">
-          <h2 className="text-black text-center font-extrabold text-2xl font-mono">
-            Welcome Back, Admin.
+          <h2 className="text-black text-center font-extrabold text-3xl font-mono">
+            Welcome
           </h2>
-          <p className="capitalize text-base  text-gray-400  text-center w-3/5 self-center">
-            Enter your details to sign in to your account.
+          <p className="capitalize text-base text-gray-400 text-center w-3/5 self-center">
+            Enter your details to create an account.
           </p>
           <div className="w-[100%] flex flex-col p-5">
             <label className="text-base">Email</label>
@@ -42,22 +42,15 @@ const page = () => {
               className="border border-gray-300 px-4 py-3 rounded-md text-sm w-full  bg-[#F8F9FA] outline-none text-black"
               placeholder="password"
             />
-            <div className="flex justify-between items-center mt-6">
-              <span className="text-sm flex items-center justify-center gap-2">
-                <input type="checkbox" />
-                <span>Remember Me</span>
-              </span>
-              <span className="text-sm">Forgot Password?</span>
-            </div>
           </div>
           <button className="bg-purple-500 w-[35%] text-base rounded-3xl p-2 text-white self-center">
-            Login
+            Sign Up
           </button>
           <p className="text-base text-center my-4">
-            New To VybesApp? {"  "}{" "}
-            <a href="/register">
+            Already Have An Account? {"  "}{" "}
+            <a href="/">
               <span className="font-bold underline text-purple-500 cursor-pointer">
-                SignUp
+                Login
               </span>
             </a>
           </p>
