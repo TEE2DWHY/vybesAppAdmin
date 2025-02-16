1c60b390374c43edad0414b095cc212f9d88887b
TEE2DWHY <isaacolorunfemi330@gmail.com>
2025-02-16 19:00:21 +0100

    started working on the registration page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,61 @@
 import Image from "next/image";
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="bg-gradient-to-r from-purple-300 to-white">
+      <Image
+        src={"/images/logo.png"}
+        alt="logo"
+        width={40}
+        height={40}
+        className="absolute top-8 left-8 cursor-pointer"
+        priority
+      />
+      <div className="flex flex-col items-center justify-center h-[100vh] font-[family-name:var(--font-geist-mono)] capitalize text-2xl">
+        <h1 className="font-semibold mb-4 text-[24px] uppercase text-center lg:text-3xl">
+          VybesApp Admin Dashboard.
+        </h1>
+        <form className="bg-white rounded-xl py-5 px-3 gap-2 flex flex-col w-[90%] sm:w-[55%] lg:w-[35%] shadow-lg">
+          <h2 className="text-black text-center font-extrabold text-2xl font-mono">
+            Welcome Back, Admin.
+          </h2>
+          <p className="capitalize text-base text-black text-center w-3/5 self-center">
+            Enter your details to get sign in to your account.
+          </p>
+          <div className="w-[100%] flex flex-col p-5">
+            <label className="text-base">Email</label>
+            <input
+              type="email"
+              className="border border-gray-300 px-4 py-3 rounded-md text-sm w-full  bg-[#F8F9FA] outline-none text-black mb-2"
+              placeholder="email"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <label className="text-base">Password</label>
+            <input
+              type="password"
+              className="border border-gray-300 px-4 py-3 rounded-md text-sm w-full  bg-[#F8F9FA] outline-none text-black"
+              placeholder="password"
+            />
+            <div className="flex justify-between items-center mt-6">
+              <span className="text-sm flex items-center justify-center gap-2">
+                <input type="checkbox" />
+                <span>Remember Me</span>
+              </span>
+              <span className="text-sm">Forgot Password?</span>
+            </div>
+          </div>
+          <button className="bg-purple-500 w-[35%] text-base rounded-3xl p-2 text-white self-center">
+            Submit
+          </button>
+          <p className="text-base text-center my-4">
+            New To VybesApp? {"  "}{" "}
+            <span className="font-bold underline text-purple-500 cursor-pointer">
+              SignUp
+            </span>
+          </p>
+        </form>
+      </div>
     </div>
   );
-}
+};
+
+export default page;
