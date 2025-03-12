b5437eb2c322585e0fcf51f1ee232a27457ca314
TEE2DWHY <isaacolorunfemi330@gmail.com>
2025-03-12 19:25:33 +0100

    updated the editEventModal styling

--- app/dashboard/components/modals/EditEventModal.tsx
+++ app/dashboard/components/modals/EditEventModal.tsx
@@ -2,50 +2,57 @@ import React from "react";
 import { MdOutlineCancel } from "react-icons/md";
 import { Event } from "@/types";
 import Image from "next/image";
-import { message } from "antd";
+import { FiEdit3 } from "react-icons/fi";
 
-interface EditUserModalProps {
+interface EditEventModalProps {
   hideEventModal: () => void;
   event: Event;
 }
 
-const EditEventModal: React.FC<EditUserModalProps> = ({
+const EditEventModal: React.FC<EditEventModalProps> = ({
   hideEventModal,
   event,
 }) => {
-  const [messageApi, contextHolder] = message.useMessage();
-
   return (
     <div className="fixed z-50 w-full h-full bg-[#1b1b1b62] flex items-center justify-center">
       <div className="bg-white w-[90%] sm:w-[32%] flex flex-col rounded-2xl px-6 py-6 shadow-lg relative">
         <div className="flex items-center justify-between w-full border-b border-gray-300 pb-2">
           <h2 className="font-semibold text-xl text-gray-800">Edit Details</h2>
           <MdOutlineCancel
-            size={24}
+            size={20}
             className="cursor-pointer text-gray-700"
             onClick={hideEventModal}
           />
         </div>
-        <div className="flex items-center gap-4 mt-4">
-          <div className="relative w-16 h-16 rounded-full overflow-hidden border-2 border-gray-300">
-            <Image
-              src={event.image}
-              alt="profile-img"
-              layout="fill"
-              objectFit="cover"
-              priority
-              unoptimized
-            />
+        <div className="flex justify-between mt-5">
+          <div className="flex items-center gap-4">
+            <div className="relative w-16 h-16 rounded-full overflow-hidden border-2 border-gray-300">
+              <Image
+                src={event.image}
+                alt="profile-img"
+                layout="fill"
+                objectFit="cover"
+                priority
+                unoptimized
+              />
+            </div>
+            <div className="flex flex-col w-[70%]">
+              <p className="text-gray-400 text-sm">
+                Choose a file or drag & drop it here
+              </p>
+              <p className="text-gray-400 text-sm">
+                JPEG, PNG, and MP4 formats, up to 10MB
+              </p>
+            </div>
           </div>
-          <div className="flex flex-col w-[70%]">
-            <p className="text-purple-600 font-semibold">Upload</p>
-            <p className="text-gray-400 text-sm">
-              Choose a file or drag & drop it here
-            </p>
-            <p className="text-gray-400 text-sm">
-              JPEG, PNG, and MP4 formats, up to 10MB
-            </p>
-          </div>
+          <input type="file" accept="image/*" id="eventImage" hidden />
+          <label
+            className="flex justify-center gap-1 cursor-pointer"
+            htmlFor="eventImage"
+          >
+            <FiEdit3 size={22} className="w-full text-purple-700" />
+            <span className="w-full text-purple-700">Edit</span>
+          </label>
         </div>
 
         <form className="mt-5 space-y-4">
@@ -55,7 +62,7 @@ const EditEventModal: React.FC<EditUserModalProps> = ({
             </label>
             <input
               type="text"
-              className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
+              className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-purple-500 capitalize"
               placeholder="Enter event name"
               defaultValue={event.name}
             />
@@ -64,18 +71,25 @@ const EditEventModal: React.FC<EditUserModalProps> = ({
             <label className="block text-gray-700 font-medium">
               Event Type
             </label>
-            <input
-              type="text"
-              className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
-              placeholder="Enter event type"
-              defaultValue={event.eventType}
-            />
+            <select
+              className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-purple-500 capitalize"
+              defaultValue={event.eventType || ""}
+            >
+              {/* Non-clickable placeholder */}
+              <option value="" disabled>
+                Select Event
+              </option>
+              <option value="Birthday Parties">Birthday Parties</option>
+              <option value="In House Party">In House Party</option>
+              <option value="Get Together">Get Together</option>
+              <option value="No Cup Parties">No Cup Parties</option>
+            </select>
           </div>
           <div>
             <label className="block text-gray-700 font-medium">Location</label>
             <input
               type="text"
-              className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
+              className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-purple-500 capitalize"
               placeholder="Enter location"
               defaultValue={event.location}
             />
@@ -96,7 +110,7 @@ const EditEventModal: React.FC<EditUserModalProps> = ({
             </label>
             <input
               type="number"
-              className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
+              className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-purple-500 capitalize"
               placeholder="Enter number of tickets purchased"
               defaultValue={event.ticketPurchased}
             />
@@ -105,7 +119,7 @@ const EditEventModal: React.FC<EditUserModalProps> = ({
             <label className="block text-gray-700 font-medium">DJs</label>
             <input
               type="text"
-              className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
+              className="w-full border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-purple-500 capitalize"
               placeholder="Enter DJs (separate multiple names with commas)"
               defaultValue={event.dj.join(", ")}
             />
